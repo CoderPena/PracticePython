76ec975b1b76e3e860d1f9363a4885eda0b7bb50
TcheloPena <86989836+TcheloPena@users.noreply.github.com>
2021-12-28 22:27:59 -0300

    Update Lab03.py

--- Lab03.py
+++ Lab03.py
@@ -1,5 +1,8 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 8,4,2,9]
-
+b = []
+x = int(input("Type a integer number: "))
 for elem in a:
-   if elem < 5:
-      print(elem)
+   if elem < x:
+      b.append(elem)
+      
+print(b)
