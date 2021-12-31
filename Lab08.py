3aad5854fdbd2857c3443b345b5503fb903fdf2b
TcheloPena <86989836+TcheloPena@users.noreply.github.com>
2021-12-31 20:51:26 -0300

    Update Lab08.py

--- Lab08.py
+++ Lab08.py
@@ -24,6 +24,14 @@ while True:
    # Prints what computer entered
    print("Computer entered : " + choices[computer_choice])
 
+   # Player1   S  P  R
+   #           2  1  0
+   #
+   # Player2   P  R  S
+   #           1  0  2
+   # (P2+1)%3  2  1  0
+   # So, if P1=P2, P1 win.
+
    if player_choice == computer_choice:
    	print('Tie!')
    elif (player_choice+1)%3 == computer_choice:
