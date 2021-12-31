4617ebdd2dda6ff9ed2d0564eb130c0339ff2eb4
TcheloPena <86989836+TcheloPena@users.noreply.github.com>
2021-12-31 20:41:31 -0300

    Update Lab08.py

--- Lab08.py
+++ Lab08.py
@@ -1,39 +1,32 @@
-while True:
-	x = int(input("type 2-4 to # of players or other # to finish: "))
-	if x == 0 or x < 2 or x > 4:
-		break
-	players = []
-	for y in range(x):
-		players.append(int(input("Player "+str(x)+", choose 1 for Rock 2 for Scissors 3 for Paper: ")))
-		x -= 1
-	print("Player #"+str(players.index(max(players))+1)+" is the winer.")
-
-
-
-
-
 import random
-import sys
 
 # Choices in a list
 # 2-Scissors
 # 1-Paper
 # 0-Rock
-choices = ["Rock", "Paper", "Scissors"]
+choices = ["Rock", "Paper","Scissors"]
 
 win_user = 0
 win_comp = 0
 tie = 0
 
-# Computer Choice - between choices
-computer_choice = random.choice(choices)
+while True:
+   # Computer Choice - between choices
+   computer_choice = random.choice([0,1,2])
 
-player_choice = int(input("choose: 0 for Rock, 1 for Paper, 2 for Scissors or other # for quit: "))
+   player_choice = int(input("Type 0 for Rock, 1 for Paper, 0 for Rock or other # for quit: "))
+   
+   if player_choice > 2:
+   	break
 
+   # Prints what user entered
+   print("User entered : " + choices[player_choice])
+   # Prints what computer entered
+   print("Computer entered : " + choices[computer_choice])
 
-
-# Prints what user entered
-print("User entered : " + str(player_choice))
-# Prints what computer entered
-print("Computer entered : " + str(computer_choice))
-
+   if player_choice == computer_choice:
+   	print('Tie!')
+   elif (player_choice+1)%3 == computer_choice:
+   	print("Computer won")
+   else:
+	   print("Player won")
